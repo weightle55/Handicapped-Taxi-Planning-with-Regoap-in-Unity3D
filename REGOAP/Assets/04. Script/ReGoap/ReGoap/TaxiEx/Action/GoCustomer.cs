32658a9acbd070b7b4503eacb87f196461bc575f
weightle55 <metalicboots@gmail.com>
2017-10-09 03:38:51 +0900

    GoEx
    GoEx change

---
+++ REGOAP/Assets/04. Script/ReGoap/ReGoap/TaxiEx/Action/GoCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class GoCustomer : ReGoapAction<string,object>{
+/*
+	protected override void Awake(){
+		base.Awake ();
+		preconditions.Set ("myPrecondition", myValue);
+		effects.Set ("myEffects", myValue);
+	}
+
+	public override void Run(IReGoapAction<string,object> previous, IReGoapAction<string,object> next,IReGoapActionSetting<string,object> settings, ReGoapState<string,object> goalState, Action<IReGoapAction<string,object>> done, Action<IReGoapAction<string,object>> fail){
+		base.Run (previous, next, goalState, done, fail);
+
+		doneCallback (this);
+	}*/
+}
---
+++ REGOAP/Assets/04. Script/ReGoap/ReGoap/TaxiEx/Action/GoEx.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoEx : MonoBehaviour
+{
+	public float h = 0.0f;
+	public float v = 0.0f;
+
+	public GameObject customertr;
+
+	private Transform tr;
+	private Transform nexttr;
+	private Vector3 godir;
+
+	public float moveSpeed=10.0f;
+	// Use this for initialization
+	void Start ()
+	{
+		tr = GetComponent<Transform> ();
+		nexttr = customertr.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		moveTo (nexttr.position);
+	}
+
+	protected void moveTo(Vector3 position){
+		godir = position - tr.position;
+		tr.Translate (godir.normalized * Time.deltaTime * moveSpeed, Space.Self);
+	}
+}
+
